25b6da57e3fc9c6402976be28be674fb89be6ec0
ELHADADJI otmane <39245156+elhaddajiOtmane@users.noreply.github.com>
2022-12-30 17:10:18 +0100

    Create Body.js

---
+++ src/components/Body.js
@@ -0,0 +1,15 @@
+const Body = () =>{
+
+    return(
+        <div>
+            <ol>
+                otmane
+                <ol>lorem</ol>
+                <ol>lorem</ol>
+                <ol>lorem</ol>
+            </li>
+
+        </div>
+    )
+}
+export default Body;
