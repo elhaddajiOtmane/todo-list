92cee3da24f3c98ed342ce04711eedd0897a6109
ELHADADJI otmane <39245156+elhaddajiOtmane@users.noreply.github.com>
2022-12-30 17:10:04 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './style/App.css';
+import NavBar from './components/Navbar'
+import Lop from './components/Body'
+import Home from './Home'
 
 function App() {
   return (
+ 
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+         <NavBar/>
+         <Home></Home>
+         <Lop></Lop>
+
     </div>
   );
 }
